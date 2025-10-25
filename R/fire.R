760cc738dc36ab6bbf90c08482a86a495433cef6
Julian Moreno <julian.k.moreno-1@ou.edu>
2025-10-25 00:46:06 -0500

    fire data

---
+++ R/fire.R
@@ -0,0 +1,13 @@
+#' World Health Organization TB data
+#'
+#' A subset of data from the World Health Organization Global Tuberculosis
+#' Report ...
+#'
+#' @format ## `fire`
+#' A data frame with 15 rows and 2 columns:
+#' \describe{
+#'   \item{DISTANCE}{distance from fire}
+#'   \item{DAMAGE}{rating of damage caused by fire}
+#'   ...
+#' }
+"fire"
